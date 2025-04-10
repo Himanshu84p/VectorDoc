938a9ef9fb8c920c746449032607b7dd4c7b2d42
Himanshu84p <himanshuprajapati8417@gmail.com>
2025-04-10 19:06:46 +0530

    fix : fixes invalid Id error by handling with regex

--- controllers/document.controller.js
+++ controllers/document.controller.js
@@ -209,6 +209,12 @@ const getDocument = async (req, res) => {
       });
     }
 
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).json({
+        status: 400,
+        message: "Invalid document Id",
+      });
+    }
     const document = await Document.findById(id);
 
     if (!document) {
